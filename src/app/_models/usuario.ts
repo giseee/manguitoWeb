0e9f1a694836dfc67dfc24aee47074ca2e26425b
fernandormd <fernandormd@gmail.com>
2023-05-02 19:37:37 -0300

    Se ajusta el formulario de edicion de usaurio. Restan ajustes

--- src/app/_models/usuario.ts
+++ src/app/_models/usuario.ts
@@ -4,10 +4,10 @@ export class Usuario {
     id: number;
     nombre: string;
     password: string;
-    perfiles: [Role];
+    perfiles: Role[];
     mail:string;
 
-    constructor(id: number=0,nombre: string = "", password: string = "", perfiles:[Role], mail:string="") {
+    constructor(id: number=0,nombre: string = "", password: string = "", perfiles:Role[]= [], mail:string="") {
         this.id = id;
         this.nombre = nombre;
         this.password = password;
--- src/app/edit-user/edit-user.component.ts
+++ src/app/edit-user/edit-user.component.ts
@@ -2,76 +2,62 @@ import { Component, OnInit } from '@angular/core';
 import {  Router } from '@angular/router';
 import { AuthenticationService } from '../_services/authentication.service';
 import { UsuarioService } from '../_services/usuario.service';
-
-
 import { Usuario } from '../_models/usuario';
+import { catchError, EMPTY, finalize, map } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
+import { User } from '../_models';
+
 @Component({
   selector: 'app-edit-user',
   templateUrl: './edit-user.component.html',
   styleUrls: ['./edit-user.component.scss']
 })
+
 export class EditUserComponent implements OnInit {
-onUpdateUser() {
-throw new Error('Method not implemented.');
-}
-
-  usuario:Usuario={
-    nombre:'',
-    id:0,
-    password:'',
-    perfiles: ['ROLE_USER'],
-    mail:''
-  };
-  id!: number;
   editing: boolean = false;
-  usuarioToEdit: Usuario={
-    nombre:'',
-    id:0,
-    password:'',
-    perfiles: ['ROLE_USER'],
-    mail:''
-  };
+  usuarioToEdit!: Usuario | null;
   constructor(
-
     private router: Router,
     private usuarioService: UsuarioService,
     private authService: AuthenticationService
   ) { }
 
   ngOnInit() {
-    this.usuarioToEdit.id=this.authService.getCurrentUserId();
+    this.authService.currentUsuario$.pipe(
+      map((user) => {
+        this.usuarioToEdit = user;
+        console.log(user);
+      })
+    ).subscribe();
   }
 
 
   onUpdateUsuario(): void {
-    this.usuarioService.updateUsuario(this.usuarioToEdit)
-      .subscribe(() => {
-        this.usuario = this.usuarioToEdit;
-        this.editing = false;
-        this.usuarioToEdit!;
-      });
-  }
-
-  getUsuarioId() {
-    this.authService.currentUsuario$.subscribe(data => {
-      this.usuario.id = data?.id??0;
-    }); // Obtener el ID del usuario desde el servicio de autenticación
-  };
-
-/* updateUsuario() {
-
-    this.id.usuario=this.authService.getCurrentUser()?.id_usuario
-    console.log(this.usuario.id_usuario);
-    this.usuarioService.putUsuarioById(this.usuario.id_usuario, this.usuario)
-      this.router.navigate(['/']);
-  }*/
-  editUsuario(usuario: Usuario): void {
     this.editing = true;
-    this.usuarioToEdit = Object.assign({}, usuario);
+    this.usuarioService.updateUsuario(this.usuarioToEdit!)
+    .pipe(// Actualizar el usuario gaurdado en el navegador
+      finalize(() => {
+        this.editing = false; 
+        this.router.navigateByUrl('/dashboard')
+      }),
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === 401) {
+          this.handleUnauthorized();
+          return EMPTY;
+        }
+        throw error;
+      })
+    ).subscribe();    
   }
+
+  handleUnauthorized() {
+    //******qué hacemos acá????**** */
+    // this.form.setErrors({ invalidCredentials: true });
+    // this.cdr.markForCheck();
+  }
+
   cancelEdit(): void {
     this.editing = false;
-
     this.router.navigateByUrl('/dashboard')
   }
 }
