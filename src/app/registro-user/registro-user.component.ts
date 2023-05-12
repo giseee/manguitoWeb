43b06ebdcffc5e470efad9c32bdae831f8ea74a3
fernandormd <fernandormd@gmail.com>
2023-05-12 15:43:53 -0300

    Ajustes en formulario de registro de usuario.

--- src/app/registro-user/registro-user.component.ts
+++ src/app/registro-user/registro-user.component.ts
@@ -3,6 +3,10 @@ import { NgForm } from '@angular/forms';
 import { User } from '../_models/user';
 import { UsuarioService } from '../_services/usuario.service';
 import { Router } from '@angular/router';
+import { EMPTY, catchError, finalize } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
+import { AlertService } from '../_alert';
+
 @Component({
   selector: 'app-registro-user',
   templateUrl: './registro-user.component.html',
@@ -12,26 +16,50 @@ export class RegistroUserComponent {
   user: User = {
     nombre: '', password: ''
   };
-  mail:string="";
-  router: any;
+  mail:string="";  
   registrando=false;
 
-  constructor(private userService: UsuarioService) { }
+  constructor(
+    private userService: UsuarioService,
+    public alertService: AlertService,
+    private router: Router
+    ) { }
 
   onSubmit(form: NgForm) {
 
     this.registrando=true;
     if (form.valid) {
-      this.userService.crearUsuario({ usuario: this.user }).subscribe(
-        () => {
-          this.registrando=true;
-          console.log('Usuario registrado con éxito');
-        }
-      );
+      this.userService.crearUsuario({ usuario: this.user }).pipe(
+        finalize(
+          () => {
+            this.registrando = false; 
+            this.router.navigateByUrl('/');
+            this.handleSucceed();
+          }),
+        catchError((error: HttpErrorResponse) => {
+          if (error.status === 409) {
+            this.handleConflict();
+            return EMPTY;
+          }
+          throw error;
+        })
+      ).subscribe();
     }
   }
-  login(){
-    this.router.navigate(['/login']);
+  handleSucceed() {
+    let options = {
+      autoClose: true,
+      keepAfterRouteChange: false
+  };
+    this.alertService.success('El usuario se ha registrado de forma exitosa.', options);
+  }
+
+  handleConflict() {
+    let options = {
+      autoClose: true,
+      keepAfterRouteChange: false
+  };
+    this.alertService.error('El nombre de usuario ya existe en el sistema', options);
   }
 }
 
