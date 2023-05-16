59183f683335a3494f3e4dbf85210e840609ba2f
GiselaDS <67863768+giseee@users.noreply.github.com>
2023-05-16 17:51:01 -0300

    Update donaciones.ts

--- src/app/_models/donaciones.ts
+++ src/app/_models/donaciones.ts
@@ -1,8 +1,20 @@
 export class Donaciones {
-  nombre:string;
-  donacion:number;
-  constructor(nombre:string="", monto:number=0){
-    this.nombre=nombre;
-    this.donacion=monto;
+  nombreDonador: string;
+  contacto?: string;
+  mensaje?: string;
+  cantidadManguitos: number;
+  fecha: Date;
+  plan_id?: number;
+  emprendimiento_id: number;
+
+  constructor(
+    nombreDonador: string = '',
+    cantidadManguitos: number = 0,
+    emprendimiento_id: number = 0
+  ) {
+    this.nombreDonador = nombreDonador;
+    this.cantidadManguitos = cantidadManguitos;
+    this.fecha = new Date();
+    this.emprendimiento_id = emprendimiento_id;
   }
 }
