03b59035c456edd5abee5e7c9881b8041965bf66
Gisela Dos santos <gisee_cb@hotmail.com>
2023-05-16 15:37:42 -0300

    agrego ventana emergente al donar

--- src/app/detalle-emprendimiento/detalle-emprendimiento.component.ts
+++ src/app/detalle-emprendimiento/detalle-emprendimiento.component.ts
@@ -7,7 +7,7 @@ import { Usuario } from '../_models/usuario';
 import { EmprendimientoService } from '../_services/emprendimiento.service';
 import { environment as env } from 'src/environments/environments';
 import { HttpClient } from '@angular/common/http';
-
+import * as bootstrap from 'bootstrap';
 @Component({
   selector: 'app-detalle-emprendimiento',
   templateUrl: './detalle-emprendimiento.component.html',
@@ -54,5 +54,27 @@ export class DetalleEmprendimientoComponent implements OnInit {
       this.redeSociales = response;
     });
   }
+  confirmarDonacion() {
+    const manguitosInput = document.getElementById('manguitosInput') as HTMLInputElement;
+    const nombreInput = document.getElementById('nombreInput') as HTMLInputElement;
+    const emailInput = document.getElementById('emailInput') as HTMLInputElement;
+
+    const manguitos = Number(manguitosInput.value);
+    const nombre = nombreInput.value;
+    const email = emailInput.value;
+
+    // Aquí puedes procesar los datos de donación y realizar las acciones necesarias
+    // por ejemplo, aumentar los manguitos, guardar el nombre y el correo electrónico, etc.
+
+    // Restablecer los valores de los campos
+    manguitosInput.value = '';
+    nombreInput.value = '';
+    emailInput.value = '';
+
+    // Cerrar la ventana emergente
+    const donationModal = new bootstrap.Modal(document.getElementById('donationModal')||"");
+    donationModal.hide();
+  }
+
 
 }
